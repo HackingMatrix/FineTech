4289384bbeab951ca55d7231f4534feb7cee9227
Omar Cerpa <cerpa_om@hotmail.com>
2023-10-24 16:44:59 -0600

    url3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
-import HeroImage from './components/HeroImage'
-import Hero from './components/Hero'
-import Pie from './components/footer'
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import Multi from './components/Multi'
-import Thx from '/components/Thx'
-import Hero2 from '/components/Hero2'
-import Register from '/components/Register'
+import Thx from './components/Thx'
+import Hero2 from './components/Hero2'
+import Register from './components/Register'
 
 function App() {
   return (
