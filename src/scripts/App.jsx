09659ad1abf15e97536fc18004757234f6c7801a
Omar Cerpa <cerpa_om@hotmail.com>
2023-10-24 18:29:07 -0600

    jesushelpme

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,10 +1,10 @@
 
-import './App.css'
+import '../App.css'
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
-import Multi from './components/Multi'
-import Thx from './components/Thx'
-import Hero2 from './components/Hero2'
-import Register from './components/Register'
+import Multi from '../components/Multi'
+import Thx from '../components/Thx'
+import Hero2 from '../components/Hero2'
+import Register from '../components/Register'
 
 function App() {
   return (
